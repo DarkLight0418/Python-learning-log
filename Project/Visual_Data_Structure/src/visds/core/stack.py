5362dcc4beadacc1a946d7cbc1316f536a270da3
DarkLight0418 <samseong0418@naver.com>
2026-02-21 06:45:37 +0900

    [feat] 2026-02-21: 스택 코드 초기화

---
+++ Project/Visual_Data_Structure/src/visds/core/stack.py
@@ -0,0 +1,29 @@
+class EmptyStackError(Exception):
+  pass
+
+class Stack:
+  def __init__(self):
+    self._data = []
+    
+  def push(self, value):
+    self._data.append(value)
+    
+  def pop(self):
+    if self.is_empty():
+      raise EmptyStackError("스택이 비어있습니다.")
+    return self._data.pop()
+  
+  def top(self):
+    if self.is_empty():
+      raise EmptyStackError("스택이 비어있습니다.")
+    return self._data[-1]
+  
+  def is_empty(self):
+    return len(self._data) == 0
+  
+  def size(self):
+    return len(self._data)
+  
+  def snapshot(self):
+    # Presenter용 안전 복사 메소드
+    return list(self._data)
