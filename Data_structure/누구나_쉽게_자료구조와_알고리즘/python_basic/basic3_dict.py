69ca4a9feff639e09448c0a94efaddecd0fdb215
DarkLight0418 <samseong0418@naver.com>
2026-03-16 18:44:19 +0900

    [study] 2026-03-16: 딕셔너리 어떻게 사용하는지 복습함.

---
+++ Data_structure/누구나_쉽게_자료구조와_알고리즘/python_basic/basic3_dict.py
@@ -0,0 +1,16 @@
+item = dict()
+
+print("+++ 딕셔너리를 이용해 게임 아이템 입력 받기 +++")
+
+for i in range(3):
+  game_item_name = input("아이템 입력하기 >> ")
+  count = int(input("구매할 개수는? >> "))
+  item[game_item_name] = count
+  
+print("="*50)
+result = input("당신이 확인하고 싶은 아이템은 무엇입니까? >> ")
+test = item.get(result)  # key -> value
+if result in item:
+  print(f'당신이 찾는 {result}은/는 {test}개 있습니다.')
+else:
+  print(f'당신이 찾는 {result}은/는 없네요...')
