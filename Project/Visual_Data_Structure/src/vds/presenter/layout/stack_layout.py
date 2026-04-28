e2f0e0681b533f0878ae458389def92d02bb2f2f
DarkLight0418 <samseong0418@naver.com>
2026-04-28 11:20:27 +0900

    [fix] 2026-04-28: vds 일부 import 변경 수정

--- Project/Visual_Data_Structure/src/vds/presenter/layout/stack_layout.py
+++ Project/Visual_Data_Structure/src/vds/presenter/layout/stack_layout.py
@@ -5,7 +5,7 @@
 
 from __future__ import annotations
 from dataclasses import dataclass
-from Project.Visual_Data_Structure.src.vds.scene.scene_models import Vec2, RectGeom
+from vds.presenter.scene_models import Vec2, RectGeom
 
 @dataclass(frozen=True)
 class StackLayoutSpec:
