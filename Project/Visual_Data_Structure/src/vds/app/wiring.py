8ad1159f4da4d1cf01b6966418fa79dc1b0d14c6
DarkLight0418 <samseong0418@naver.com>
2026-06-19 09:21:04 +0900

    [feat] 2026-06-19: wiring.py 임시 완성

--- Project/Visual_Data_Structure/src/vds/app/wiring.py
+++ Project/Visual_Data_Structure/src/vds/app/wiring.py
@@ -2,3 +2,16 @@
 객체 연결(DI)
 Stack, Usecase, Presenter, View, Animator 연결
 '''
+
+from __future__ import annotations
+
+import tkinter as tk
+
+from vds.app.stack_panel import StackPanel
+from vds.presenter.stack_presenter import StackPresenter
+from vds.usecases.stack_usecase import StackUsecase
+
+def create_stack_panel(master: tk.Misc) -> StackPanel:
+    usecase = StackUsecase()
+    presenter = StackPresenter()
+    return StackPanel(master, usecase=usecase, presenter=presenter)
